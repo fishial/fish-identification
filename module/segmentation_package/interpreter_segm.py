6a4b99246275e9672fbfe604cc857ac84ac006e5
AndrzejCodahead <andrei.liudkievich@codahead.com>
2023-09-21 08:29:57 +0000

    update segmentation interpreter

--- module/segmentation_package/interpreter_segm.py
+++ module/segmentation_package/interpreter_segm.py
@@ -1,171 +1,275 @@
-import numpy as np
-import cv2
-import time
 import logging
-from scipy.interpolate import splprep, splev
-from detectron2.engine import DefaultPredictor
-from detectron2 import model_zoo
-from detectron2.config import get_cfg
+import time
+
+import cv2
+import numpy as np
+from PIL import Image
 from shapely.geometry import Polygon
-from detectron2.projects.point_rend import ColorAugSSDTransform, add_pointrend_config
-
-
-def get_iou_poly(polygon1, polygon2):
-    intersect = polygon1.intersection(polygon2).area
-    union = polygon1.union(polygon2).area
-    iou = intersect / union
-    return iou
-
-
-def get_area_intersection(poly_a, poly_b, threshold=0.9):
-    if poly_a.intersection(poly_b).area / poly_a.area > threshold:
-        return True
-    return False
-
+import torch
+from torch.nn import functional as F
 
 class SegmentationInference:
-    def __init__(self, model_path, device='cpu', labels_list = ['Fish']):
-        self.model_path = model_path
-        self.labels_list = labels_list
-        self.model_path = model_path
+    
+    def __init__(self, model_path):        
+        self.model = torch.jit.load(model_path)
+        self.MIN_SIZE_TEST = 800
+        self.MAX_SIZE_TEST = 1333
         
-        self.cfg = get_cfg()
-        add_pointrend_config(self.cfg)
-        config_path = "/home/fishial/Fishial/detectron2/projects/PointRend/configs/InstanceSegmentation/pointrend_rcnn_R_50_FPN_3x_coco.yaml"
+        self.SCORE_THRESHOLD = 0.3
+        self.MASK_THRESHOLD = 0.5
+        self.NMS_THRESHOLD = 0.9
         
-        self.cfg.merge_from_file(config_path)
-        self.cfg.DATALOADER.NUM_WORKERS = 2
-        self.cfg.MODEL.POINT_HEAD.NUM_CLASSES = 1
-        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
-        
-        self.cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.5
-        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.65
-        self.cfg.MODEL.DEVICE = device
-        self.cfg.MODEL.WEIGHTS = self.model_path
-        self.cfg.freeze()
-        self.model = DefaultPredictor(self.cfg)
-        self.approximate_value = 0.0004
-        
-        
-    def __NMS(self, polygons, poly):
-        for poly_id_a in range(len(polygons) - 1, -1, -1):
-            iou = get_iou_poly(polygons[poly_id_a], poly)
-            if iou > 0.5:
-                return True
-        return False
-
-    def __approximate(self, output):
-        meta_data = {
-            'polygons'    : [],
-            'scores'      : [],
-            'areas'       : [],
-            'labels_list' : []
+    def get_set_up(self):
+        return {
+            'MIN_SIZE_TEST': self.MIN_SIZE_TEST,
+            'MAX_SIZE_TEST': self.MAX_SIZE_TEST,
+            'SCORE_THRESHOLD': self.SCORE_THRESHOLD,
+            'MASK_THRESHOLD': self.MASK_THRESHOLD,
+            'NMS_THRESHOLD': self.NMS_THRESHOLD,
         }
-        poly_instances = []
-        
-        polygons_tmp = []
-
-        for z in range(len(output['instances'])):
-            masks = np.array(output['instances'].get('pred_masks')[z].to('cpu'))
-            imgUMat = np.array(masks * 255, dtype=np.uint8)
-            cnts, hierarchy = cv2.findContours(imgUMat, 1, 2)
-            cnts = sorted(cnts, key=lambda x: cv2.contourArea(x))
-            
-            if len(cnts) == 0:
-                continue
-            cnts_s = cnts[len(cnts) - 1]
-            epsilon = self.approximate_value * cv2.arcLength(cnts_s, True)
-            approx = cv2.approxPolyDP(cnts_s, epsilon, True)
-            polygons_dict = {}
-            if len(approx) < 10:
-                continue
-            try:
-                poly_tmp = Polygon([(int(approx[point_id][0][0]), int(approx[point_id][0][1])) for point_id in range(len(approx))])
-            except Exception as e:
-                print("Error! ", e)
-
-            x = []
-            y = []
-            polygons_dict = {}
-            for i in range(len(approx)):
-                x.append(int(approx[i][0][0]))
-                y.append(int(approx[i][0][1]))
-                polygons_dict.update({
-                    "x{}".format(i + 1): int(approx[i][0][0]),
-                    "y{}".format(i + 1): int(approx[i][0][1])
-                })
-             
-            meta_data['polygons'].append(polygons_dict)
-            meta_data['scores'].append(float(np.array(output['instances'].get('scores')[z].to('cpu'))))
-            meta_data['areas'].append(float(cv2.contourArea(cnts_s)))
-            meta_data['labels_list'].append(self.labels_list[int(output['instances'][z].pred_classes[0])])
-            poly_instances.append([poly_tmp, poly_tmp.area, len(meta_data['polygons']) - 1])
-        
-        poly_instances.sort(key=lambda x: x[1])
-        index_to_remove = []
-        for poly_id_a in range(len(poly_instances) -1, -1, -1):
-            for poly_id_b in range(len(poly_instances)):
-                if poly_id_a == poly_id_b: continue
-                try:
-                    if get_area_intersection(poly_instances[poly_id_a][0], poly_instances[poly_id_b][0]):
-                        index_to_remove.append(poly_instances[poly_id_a][2])
-                        break
-                except Exception as e:
-                    index_to_remove.append(poly_instances[poly_id_a][2])
-                    print("Intersection: ", e)
-                    break
-        index_to_remove.sort(reverse=True)
-        for index_ in index_to_remove:
-            for key_name in meta_data:
-                del meta_data[key_name][index_]
-        return meta_data
-
-
-    def __get_mask(self, image, pts):
-        ## (1) Crop the bounding rect
-        rect = cv2.boundingRect(pts)
-        x, y, w, h = rect
-        croped = image[y:y + h, x:x + w].copy()
-
-        ## (2) make mask
-        pts = pts - pts.min(axis=0)
-
-        mask = np.zeros(croped.shape[:2], np.uint8)
-        cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
-
-        ## (3) do bit-op
-        dst = cv2.bitwise_and(croped, croped, mask=mask)
-        return dst
-
-    def __convert_to_polygon(self, json):
-        polygon = []
-
-        for i in range(int(len(json) / 2)):
-            polygon.append([json["x{}".format(i + 1)], json["y{}".format(i + 1)]])
-        return polygon
-
-    def simple_inference(self, img):
-        start_time = time.time()
-        outputs = self.model(img)
-  
-        meta_data = self.__approximate(outputs)
-        masks = []
-        
-        for iddx, single in enumerate(meta_data['polygons']):
-            polygon = np.array(self.__convert_to_polygon(single))
-            masks.append(self.__get_mask(img, polygon))
-#         logging.info("Inference time by Mask RCNN models has taken {} [s]".format(round(time.time() - start_time, 2)))
-        return meta_data, masks
-
+    
     def re_init_model(self, threshold):
-        
         try:
-            new_threshold = max(0.65, min(0.999, threshold))
-            if new_threshold != self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST:
-                self.cfg.defrost()
-                self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = new_threshold
-                self.cfg.freeze()
-                self.model = DefaultPredictor(self.cfg)
+            self.SCORE_THRESHOLD = max(0.01, min(0.9, threshold))
         except Exception as e:
-            logger.warning('exception', extra={'custom_dimensions': {'error_message': str(e),
-                                                                     'place': 're_init_model'}})
+            logging.warning('exception', extra={'custom_dimensions': {'error_message': str(e), 'place': 're_init_model'}})
+
+    def inference(self, np_img_src):
+        start_time = time.time()
+        
+        #resize img 
+        np_img_resized = resize_img_by_shortest_endge(np_img_src, self.MIN_SIZE_TEST, self.MAX_SIZE_TEST)
+        
+        #get scales of x&y after scaling
+        scales = np.divide(np_img_src.shape[:2], np_img_resized.shape[:2])
+        
+        img_torch_tensor = torch.as_tensor(np_img_resized.astype("float32").transpose(2, 0, 1))
+        
+        segm_output = self.model(img_torch_tensor)
+        mask_and_poly = self.convert_output_to_mask_and_polygons(segm_output, np_img_resized, scales)
+        polygons, masks = self.__process_output(mask_and_poly)
+        logging.info("Inference time by Mask RCNN models has taken {} [s]".format(round(time.time() - start_time, 2)))
+        
+        return polygons, masks
+    
+    def __process_output(self, output):
+        
+        def is_valid_polygon(polygon_array):
+            try:
+                Polygon(polygon_array)
+                return True
+            except Exception as e:
+                logging.info(f"[PROCESSING][SEGMENTATION] polygon is broken for current mask - skip it: {e}")
+                return False
+
+        poly_instances = [[Polygon(polygon_array), polygon_array, mask] for mask, polygon_array in output if is_valid_polygon(polygon_array)]
+        
+        poly_instances = sorted(poly_instances, key=lambda x: x[0].area, reverse=True)
+        # Create a list of indices to keep
+        keep_indices = [0]
+
+        for i in range(1, len(poly_instances)):
+            keep = True
+            for j in keep_indices:
+                keep = self.filter_by_iou_threshold(poly_instances[i][0], poly_instances[j][0])
+            if keep:
+                keep_indices.append(i)
+
+        logging.info(f"[PROCESSING][SEGMENTATION] After applying custom NMS method removed N = {(len(poly_instances) - len(keep_indices))} indexes")
+        
+        polygons = [SegmentationInference.poly_array_to_dict(poly_instances[i][1]) for i in keep_indices]
+        masks = [poly_instances[i][2] for i in keep_indices]
+        
+        return polygons, masks
+    
+    def convert_output_to_mask_and_polygons(self, mask_rcnn_output, np_img_resized, scales):
+
+        def rescale_polygon_to_src_size(poly, start_pont, scales):
+            return [[int((start_pont[0] + point[0]) * scales[0]), 
+                     int((start_pont[1] + point[1]) * scales[1])] for point in poly]
+
+        boxes, classes, masks, scores, img_size = mask_rcnn_output
+        processed = []
+
+        for ind in range(len(masks)):
+            if scores[ind] <= self.SCORE_THRESHOLD: continue
+            x1, y1, x2, y2 = int(boxes[ind][0]), int(boxes[ind][1]), int(boxes[ind][2]) , int(boxes[ind][3])  
+            mask_h, mask_w = y2 - y1, x2 - x1
+
+            np_mask = do_paste_mask(masks[ind, None, :, :], mask_h, mask_w).numpy()[0][0]
+
+            # Threshold the mask converting to uint8 casuse opencv diesn't allow other type! 
+            np_mask = np.where(np_mask > self.MASK_THRESHOLD, 255, 0).astype(np.uint8)
+            crop_image = np_img_resized[y1:y1 + mask_h, x1:x1+mask_w]
+            res = cv2.bitwise_and(crop_image, crop_image, mask = np_mask)
+
+            # Find contours in the binary mask
+            contours = bitmap_to_polygon(np_mask)
+
+            # Ignore empty contpurs and small artifacts 
+            if len(contours) < 1 or len(contours[0]) < 10:
+                continue
+
+            # Convert local polygon to src image
+            polygon_full = rescale_polygon_to_src_size(contours[0], (x1, y1), scales)
+
+            processed.append([res, polygon_full])
+        return processed
+    
+    @staticmethod
+    def poly_array_to_dict(poly):
+        polygons_dict = {}
+        for i in range(len(poly)):
+            polygons_dict.update({
+                "x{}".format(i + 1): poly[i][0],
+                "y{}".format(i + 1): poly[i][1]
+            })
+        return polygons_dict
+    
+    def filter_by_iou_threshold(self, poly_a, poly_b):
+        """
+        Checks if the two given polygons intersect.
+
+        Args:
+            - poly_a: shapely.geometry.Polygon, first polygon
+            - poly_b: shapely.geometry.Polygon, second polygon
+            - threshold: float, threshold above which the polygons intersect
+
+        Returns:
+            - True if the polygons intersect, False otherwise
+        """
+        intersection_area = poly_a.intersection(poly_b).area
+        union_area = poly_a.union(poly_b).area
+
+        iou = intersection_area / union_area
+        if iou > self.NMS_THRESHOLD:
+            return False
+        return True
+
+# Image utils
+def resize_img_by_shortest_endge(img_np, MIN_SIZE_TEST, MAX_SIZE_TEST):
+    
+    src_h, src_w = img_np.shape[:2]
+    new_h, new_w = get_output_shape(src_h, src_w, MIN_SIZE_TEST, MAX_SIZE_TEST)
+    img_resized_np = apply_image(img_np, new_h, new_w)
+    
+    return img_resized_np
+
+def get_output_shape(
+    oldh: int, oldw: int, short_edge_length: int, max_size: int) -> [int, int]:
+    """
+    Compute the output size given input size and target short edge length.
+    """
+    h, w = oldh, oldw
+    size = short_edge_length * 1.0
+    scale = size / min(h, w)
+    if h < w:
+        newh, neww = size, scale * w
+    else:
+        newh, neww = scale * h, size
+    if max(newh, neww) > max_size:
+        scale = max_size * 1.0 / max(newh, neww)
+        newh = newh * scale
+        neww = neww * scale
+    neww = int(neww + 0.5)
+    newh = int(newh + 0.5)
+    return (newh, neww)
+
+def apply_image(img, new_h, new_w, interp_method=Image.BILINEAR):
+    assert len(img.shape) <= 4
+    
+    if img.dtype == np.uint8:
+        if len(img.shape) > 2 and img.shape[2] == 1:
+            pil_image = Image.fromarray(img[:, :, 0], mode="L")
+        else:
+            pil_image = Image.fromarray(img)
+        pil_image = pil_image.resize((new_w, new_h), interp_method)
+        ret = np.asarray(pil_image)
+        if len(img.shape) > 2 and img.shape[2] == 1:
+            ret = np.expand_dims(ret, -1)
+    else:
+        # PIL only supports uint8
+        if any(x < 0 for x in img.strides):
+            img = np.ascontiguousarray(img)
+        img = torch.from_numpy(img)
+        shape = list(img.shape)
+        shape_4d = shape[:2] + [1] * (4 - len(shape)) + shape[2:]
+        img = img.view(shape_4d).permute(2, 3, 0, 1)  # hw(c) -> nchw
+        _PIL_RESIZE_TO_INTERPOLATE_MODE = {
+            Image.NEAREST: "nearest",
+            Image.BILINEAR: "bilinear",
+            Image.BICUBIC: "bicubic",
+        }
+        mode = _PIL_RESIZE_TO_INTERPOLATE_MODE[interp_method]
+        align_corners = None if mode == "nearest" else False
+        img = F.interpolate(
+            img, (new_h, new_w), mode=mode, align_corners=align_corners
+        )
+        shape[:2] = (new_h, new_w)
+        ret = img.permute(2, 3, 0, 1).view(shape).numpy()  # nchw -> hw(c)
+
+    return ret
+
+def do_paste_mask(masks, img_h: int, img_w: int):
+    """
+    Args:
+        masks: N, 1, H, W
+        boxes: N, 4
+        img_h, img_w (int):
+        skip_empty (bool): only paste masks within the region that
+            tightly bound all boxes, and returns the results this region only.
+            An important optimization for CPU.
+
+    Returns:
+        if skip_empty == False, a mask of shape (N, img_h, img_w)
+        if skip_empty == True, a mask of shape (N, h', w'), and the slice
+            object for the corresponding region.
+    """
+    device = masks.device
+
+    x0_int, y0_int = 0, 0
+    x1_int, y1_int = img_w, img_h
+    x0, y0, x1, y1 =  torch.Tensor([[0]]), torch.Tensor([[0]]), torch.Tensor([[img_w]]), torch.Tensor([[img_h]])
+
+    N = masks.shape[0]
+
+    img_y = torch.arange(y0_int, y1_int, device=device, dtype=torch.float32) + 0.5
+    img_x = torch.arange(x0_int, x1_int, device=device, dtype=torch.float32) + 0.5
+    img_y = (img_y - y0) / (y1 - y0) * 2 - 1
+    img_x = (img_x - x0) / (x1 - x0) * 2 - 1
+    # img_x, img_y have shapes (N, w), (N, h)
+    gx = img_x[:, None, :].expand(N, img_y.size(1), img_x.size(1))
+    gy = img_y[:, :, None].expand(N, img_y.size(1), img_x.size(1))
+    grid = torch.stack([gx, gy], dim=3)
+
+    resized_mask = F.grid_sample(masks, grid.to(masks.dtype), align_corners=False)
+
+    return resized_mask
+    
+def bitmap_to_polygon(bitmap):
+    """Convert masks from the form of bitmaps to polygons.
+
+    Args:
+        bitmap (ndarray): masks in bitmap representation.
+
+    Return:
+        list[ndarray]: the converted mask in polygon representation.
+        bool: whether the mask has holes.
+    """
+    bitmap = np.ascontiguousarray(bitmap).astype(np.uint8)
+    # cv2.RETR_CCOMP: retrieves all of the contours and organizes them
+    #   into a two-level hierarchy. At the top level, there are external
+    #   boundaries of the components. At the second level, there are
+    #   boundaries of the holes. If there is another contour inside a hole
+    #   of a connected component, it is still put at the top level.
+    # cv2.CHAIN_APPROX_NONE: stores absolutely all the contour points.
+    outs = cv2.findContours(bitmap, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    contours = outs[-2]
+    hierarchy = outs[-1]
+    if hierarchy is None:
+        return [], False
+    # hierarchy[i]: 4 elements, for the indexes of next, previous,
+    # parent, or nested contours. If there is no corresponding contour,
+    # it will be -1.
+    contours = [c.reshape(-1, 2) for c in contours]
+    return sorted(contours, key=len, reverse = True)
